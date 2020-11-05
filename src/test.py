fd68279db702aa5c7c2744cab113e9007adf6678
hluo76 <robin1997@gatech.edu>
2020-11-04 21:30:30 -0500

    v 0.0.1

--- src/test.py
+++ src/test.py
@@ -19,10 +19,12 @@ def readfile(args):
             for line in f:
                 l = line.split(" ")
                 for i in l:
-                    mst[index].append(int(i))     
+                    if i  !='\n':
+                        graph[index].append(int(i))    
+                index += 1 
     else:
         raise FileNotFoundError('Please Inpyut data file')
-    return mst
+    return graph
 
 
 def output(args):
@@ -43,6 +45,7 @@ parser.add_argument('-out_sol', type=argparse.FileType('w'), dest = 'out_sol')
 parser.add_argument('-out_trace', type=argparse.FileType('w'), dest = 'out_trace')
 args = parser.parse_args()
 mst = readfile(args)
+print(mst)
 if args.alg == 'BnB':
     pass
 elif args.alg == 'Approx':
