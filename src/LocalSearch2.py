c423d90bc310edc6a075627a5e4564b3a591f8ec
hluo76 <robin1997@gatech.edu>
2020-11-09 16:01:08 -0500

    v1.0.0

--- src/LocalSearch2.py
+++ src/LocalSearch2.py
@@ -1,2 +1,24 @@
 def LS2(graph, time, seed, out_sol = False, out_trace = False):
     pass
+
+
+'''
+def readfile(filename):
+    with open(filename, "r") as f:
+        first_line = f.readline()
+        num_vertrix = int(first_line.split(" ")[0])
+        num_edge = int(first_line.split(" "
+        )[1])
+        opt = int(first_line.split(" ")[2])
+        graph = defaultdict(list)
+        index = 1
+        for line in f:
+            l = line.split(" ")
+            for i in l:
+                if i  !='\n':
+                    graph[index].append(int(i))    
+            index += 1 
+    return graph
+graph = readfile('../DATA/football.graph')
+LS2(graph, 3, 5, out_sol = False, out_trace = False)
+'''
