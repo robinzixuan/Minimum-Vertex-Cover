911462f05c47e8f67a1e22068cba23b0819a17cd
hluo76 <robin1997@gatech.edu>
2020-11-09 16:02:07 -0500

    v1.0.0

--- src/BranchAndBound.py
+++ src/BranchAndBound.py
@@ -1,3 +1,25 @@
 def BranchAndBound(graph, time, seed, out_sol = False, out_trace = False):
     pass
     
+
+
+'''
+def readfile(filename):
+    with open(filename, "r") as f:
+        first_line = f.readline()
+        num_vertrix = int(first_line.split(" ")[0])
+        num_edge = int(first_line.split(" "
+        )[1])
+        opt = int(first_line.split(" ")[2])
+        graph = defaultdict(list)
+        index = 1
+        for line in f:
+            l = line.split(" ")
+            for i in l:
+                if i  !='\n':
+                    graph[index].append(int(i))    
+            index += 1 
+    return graph
+graph = readfile('../DATA/football.graph')
+BranchAndBound(graph, 3, 5, out_sol = False, out_trace = False)
+'''
