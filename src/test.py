85c92f359245ca84cb8a7c8cfabbd80b1ec11d43
hluo76 <robin1997@gatech.edu>
2020-11-10 13:50:35 -0500

    v1.0.1

--- src/test.py
+++ src/test.py
@@ -3,8 +3,8 @@ import numpy
 import time
 from BranchAndBound import BranchAndBound
 from Approximate import Approximate
-from LocalSearch1 import LS1
-from LocalSearch2 import LS2
+from LocalSearch1 import LS1_SA
+from LocalSearch2 import LS2_HC
 import sys
 import heapq
 from collections import deque, defaultdict 
@@ -29,10 +29,17 @@ def readfile(args):
                 index += 1 
     else:
         raise FileNotFoundError('Please Inpyut data file')
-    return graph
+    return  graph,vertices
 
 
-
+def writefile(dicts, filename, sol, trace):
+    sol_filename = dicts + filename +'.sol'
+    trace_filename = dicts + filename +'.trace'
+    with open(sol_filename,'w') as f:
+        f.write(str(len(sol))+'\n')
+        f.write(str(sol)[1:-1])
+    with open(trace_filename,'w') as f:
+        pass
 
 
 
@@ -41,50 +48,29 @@ parser.add_argument('-inst', type=argparse.FileType('r'), help='Data for Test.',
 parser.add_argument('-alg', action='store', dest = 'alg', help='Alg Name')
 parser.add_argument('-time', action='store', dest = 'time',type=int, help='cutoff time in second')
 parser.add_argument('-seed', action="store", dest="seed", type=int, help='Random seed')
-parser.add_argument('-out_sol', type=argparse.FileType('w'), dest = 'out_sol')
-parser.add_argument('-out_trace', type=argparse.FileType('w'), dest = 'out_trace')
+
 args = parser.parse_args()
-graph = readfile(args)
+graph,vertices  = readfile(args)
+
+filename = args.data.name.split('/')[-1]
+filename = filename  + '_' + args.alg + '_' + str(args.time)
+dicts = '../Sol/'
 if args.alg == 'BnB':
+    filename +=  '_' + str(args.seed)
     sol, trace = BranchAndBound(graph,vertices, args.time, args.seed)
-    if parser.out_sol and parser.out_trace:
-        pass
-    elif parser.out_sol:
-        pass
-    elif parser.out_trace:
-        pass
-    else:
-        pass
+    writefile(dicts, filename, sol, trace)
 elif args.alg == 'Approx':
     sol, trace = Approximate(graph, vertices, args.time, args.seed)
-    if parser.out_sol and parser.out_trace:
-        pass
-    elif parser.out_sol:
-        pass
-    elif parser.out_trace:
-        pass
-    else:
-        pass
+    writefile(dicts, filename, sol, trace)
 elif args.alg == 'LS1':
+    filename +=  '_' + str(args.seed)
     sol, trace = LS1_SA(graph, vertices, args.time, args.seed)
-    if parser.out_sol and parser.out_trace:
-        pass
-    elif parser.out_sol:
-        pass
-    elif parser.out_trace:
-        pass
-    else:
-        pass
+    writefile(dicts, filename, sol, trace)
 elif args.alg == 'LS2':
+    filename +=  '_' + str(args.seed)
     sol, trace = LS2_HC(graph, vertices, args.time, args.seed)
-    if parser.out_sol and parser.out_trace:
-        pass
-    elif parser.out_sol:
-        pass
-    elif parser.out_trace:
-        pass
-    else:
-        pass
+    writefile(dicts, filename, sol, trace)
+    
 
 
 
