eee792b03bd6c29b5f8bc91a22abdd938b6fa6ed
hluo76 <robin1997@gatech.edu>
2020-11-17 12:33:59 -0500

    v3.0.1

--- src/LocalSearch2.py
+++ src/LocalSearch2.py
@@ -172,27 +172,6 @@ def init_vc(graph, vertices):
     temp = sorted(temp)
     return temp
 
-def readfile(filename):
-    with open(filename, "r") as f:
-        first_line = f.readline()
-        num_vertrix = int(first_line.split(" ")[0])
-        num_edge = int(first_line.split(" ")[1])
-        weight = int(first_line.split(" ")[2])
-        graph = defaultdict(list)
-        vertices = set()
-        index = 1
-        for line in f:
-            l = line.split(" ")
-            for i in l:
-                if i  !='\n':
-                    graph[index].append(i)   
-                    vertices.add(i)
-            index += 1 
-    return graph,vertices
-filename = '../DATA/football.graph'
-graph, vertices = readfile(filename)
-HillClimbing(graph, vertices, 60, 1045)
-# print ('Solution size is', len(solution))
     
     
 
