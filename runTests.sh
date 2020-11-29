35134abdd9b412f9262a5b064023ad1f8a00c863
jlinhaha <brucelinst@gmail.com>
2020-11-29 16:02:56 -0500

    bnb sol
    commit bnb sol

--- runTests.sh
+++ runTests.sh
@@ -7,7 +7,7 @@ for graph in ${graphFiles}
 do
 	filename=`echo ${graph} | cut -d'.' -f1`
 	echo ${graph} ${filename}
-	python3 ./src/test.py -inst ./data/${graph}  -alg LS1 -time 30 -seed 5
+	python3 ./src/test.py -inst ./data/${graph}  -alg BnB -time 30 -seed 5
 
 
 done
--- src/BranchAndBound.py
+++ src/BranchAndBound.py
@@ -1,72 +1,89 @@
 import time
-import copy
+import math
+import sys
+
 
 def BranchAndBound(graph, vertices, cutoff_time, num_edge):
     start_time = time.time()
-    Cover = set()
-    UB = vertices
+    current_best = vertices
+    uppper_bound = len(graph)
     trace = []
-    Cover, traace = EMVC(graph, UB, Cover, trace, vertices, cutoff_time, num_edge)
-    pass
+
+    # the large graph will lead recursion error: maximum recursion depth exceeded in comparison
+    # so we need to set recursion limit
+    if sys.getrecursionlimit() < uppper_bound:
+        sys.setrecursionlimit(uppper_bound + 2)
+
+    def backtracking(cover, cover_num, subgraph, subgraph_num_edge):
+        if time.time() - start_time > cutoff_time:
+            # timeout
+            return
+
+        nonlocal current_best, trace, uppper_bound
+        # if there is no edge on current subgraph, there will search until status
+        if subgraph_num_edge == 0:
+            if len(cover) < uppper_bound:
+                cost = time.time() - start_time
+                current_best = [k for k, v in cover.items() if v]
+                print(len(current_best), cost)
+                uppper_bound = len(current_best)
+                trace.append(str(cost) + ' ' + str(uppper_bound))
+            return
+
+        # vertex <- findHighestUncoveredDegree
+        vertex, max_degree = findHighestUncoveredDegree(graph, cover)
+        lb = math.ceil(subgraph_num_edge / max_degree)
+        if cover_num + lb >= uppper_bound:
+            # if current_used_vertex + lb >= current_best then pruning
+            return
+
+        # vertex is used
+        cover[vertex] = True
+        edges = subgraph[vertex]
+        # remove the edge of the vertex
+        for neighbor in edges:
+            subgraph[neighbor].remove(vertex)
+        del subgraph[vertex]
+
+        # backtracking
+        backtracking(cover, cover_num + 1, subgraph, subgraph_num_edge - len(edges))
+
+        # vertex is unused
+        # the vertex has been choosed, the vertex connected to that vertex has to be choosen
+        # it not, the edge won't be covered
+        new_cover = cover.copy()
+        new_cover[vertex] = False
+        for neighbor in edges:
+            new_cover[neighbor] = True
+        new_cover_num = len([k for k, v in new_cover.items() if v])
+   
+        # backtracking
+        backtracking(new_cover, new_cover_num, subgraph, subgraph_num_edge - len(edges))
+
+        # restore the edge of subgraph
+        subgraph[vertex] = edges
+        for neighbor in edges:
+            subgraph[neighbor].add(vertex)
+        del cover[vertex]
+
+    # put the vertex connected to k into set so that we can modify on backtracking
+    graph = {k: set([int(i) for i in v]) for k, v in graph.items()}
+    backtracking({}, 0, graph, num_edge)
+    return current_best, trace
 
 
+def findHighestUncoveredDegree(graph, cover):
+    '''
+    find the maximum degree of vertex that has not been covered
+    '''
+    best_vertex, max_degree = None, None
+    for k, v in graph.items():
+        if k not in cover and len(v) > 0:
+            if max_degree is None or max_degree < len(v):
+                max_degree = len(v)
+                best_vertex = k
 
-def EMVC(graph, UB, C,trace, cutoff_time, num_edge):
-    while time.time() - start_time < cutoff_time:
-        if len(C) + DegLB(graph) >= len(UB):
-            return vertices, traace
-        if graph is empty:
-            return Cover, traace
-        v = max(graph, key = lambda k: len(graph[k])) 
-        graph_copy = copy.deepcopy(graph)
-        graph_copy_1 = copy.deepcopy(graph) 
-        C_copy = copy.deepcopy(C)
-        C_copy_1 = copy.deepcopy(C)
-        del graph_copy_1[v]
-        del graph_copy[v]
-        for key in graph:
-            del graph_copy[key]
-            C_copy.add(key)
-        C1, trace = EMVC(graph_copy, UB, C_copy,trace, cutoff_time, num_edge)
-        C2, trace = EMVC(graph_copy_1, find_min(UB,C1), C_copy_1.add(v), trace, cutoff_time, num_edge)
-        return find_min(C1,C2), trace
-    return find_min(C1,C2), trace
-
-        
-
-def find_min(A,B):
-    if len(A) <= len(B):
-        return A
-    else:
-        return B
-
-
-def DegLB(G):
-    total_degree = 0
-    start = max(G, key = lambda k: len(G[k]))
-    num_edge = 0
-    for key, value in G:
-        num_edge += len(value)
-    count = 0
-    while total_degree < num_edge:
-        del G[start]
-        start = max(G, key = lambda k: len(G[k]))
-        total_degree += len(G[start])
-        count += 1
-    del G[start]
-    start = max(G, key = lambda k: len(G[k]))
-    num_edge = 0
-    for key, value in G:
-        num_edge += len(value)
-    return int(count +  num_edge/len(G[start]))
-
-
-
-
-
-
-
-
+    return best_vertex, max_degree
 
 
 '''
